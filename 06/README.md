a7923db2364977346b8f492bbe11c4bcbb904d51
James Robinson <jamesrobin11@icloud.com>
2022-12-06 14:02:21 +0000

    Compleated day 06

---
+++ 06/README.md
@@ -0,0 +1,6 @@
+# Advent of Code 2022 - Day 06
+## (Tuning Trouble)
+
+### [`main1.py`](main1.py) and [`main2.py`](main2.py)
+Loops though the string provided with a set range of `n` charecters, checks for duplicates, and if none found then it will print out `index + n`
+
---
+++ 06/main2.py
@@ -0,0 +1,30 @@
+# Advent of Code 2022 - Day 06 Solution - main2.py
+# Copyright James Robinson 2022
+# All rights (as well as lefts) reserved.
+
+from jimbotime import tim
+
+n = 14
+
+
+@tim.tim
+def main():
+    with open("input.txt") as f:
+        lines = f.readlines()
+
+    line = lines[0]
+
+    for i in range(len(line) - n):
+        flag = False
+        sub = line[i:i + n]
+        for s in sub:
+            if sub.count(s) > 1:
+                flag = True
+                break
+
+        if not flag:
+            print(i + n)
+
+
+if __name__ == "__main__":
+    main()
